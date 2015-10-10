5673a421ebd8a8b21ac5fbc48455cbc66dad81eb
thor2166 <thor2166@hotmail.com>
2015-10-10 13:06:26 +0200

    ddd

---
+++ first_app/masin.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main() {
+
+	int thor_int; //Int er lige tal
+
+	float thor_float; //Float er komma tal
+
+	double thor_double; //Double er komma tal der kan være mindere
+
+	bool thor_boolean; //Boolean fylder 1 bit
+
+	char thor_char = 'f';
+
+	std::cout << thor_char << std::endl;
+
+	system("pause");
+
+	return 0;
+}
