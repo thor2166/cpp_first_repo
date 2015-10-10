a64dff51d10cbd983f67ebdd2d581c5a6ed0b65e
thor2166 <thor2166@hotmail.com>
2015-10-10 12:25:16 +0200

    Uploaded my test solution and project

---
+++ first_app/first_app/masin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+	int thor_int;
+
+	std::cin >> thor_int;
+
+	std::cout << "here er dit tal: " << thor_int << " , thor_int fylder " << sizeof(thor_int) << " bits i din computers hukommelse" << std::endl;
+
+	system("pause");
+
+	return 0;
+}
